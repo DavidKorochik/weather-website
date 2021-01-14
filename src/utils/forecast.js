ea912bf0d43a39a679d72915a8b444e425d69a4e
DavidKorochik <david1korochik@gmail.com>
2021-01-14 22:05:26 +0200

    Bug fix in forecast.js 3.0

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find your search. Try searching again', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out and it feels like ' + body.current.feelslike + ' degrees out and the humidity is ' + body.current.weather_descriptions[6]);
+            console.log();
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out and it feels like ' + body.current.feelslike + ' degrees out and the humidity is ' + body.current.humidity);
         }
     });
 };
